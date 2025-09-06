1eb58fd3c6aaa7ca70364b8f8ab3c1b6b2991230
Sachini Imbulagoda <sachiniimbulagoda@gmail.com>
2025-09-07 02:06:09 +0530

    Spring Boot Application is created.

---
+++ Back_End/src/main/java/lk/ijse/gdse/back_end/Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
